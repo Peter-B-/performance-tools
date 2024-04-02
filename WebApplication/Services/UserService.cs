23f73b13364e9b7d078f7e28df6e92cc2655ecf0
Peter-B- <peter@butzhammer.de>
2024-04-02 21:40:41 +0200

    read users from file

--- WebApplication/Services/UserService.cs
+++ WebApplication/Services/UserService.cs
@@ -2,23 +2,57 @@ using Faker;
 
 namespace WebApplication.Services;
 
-class UserService : IUserService
+internal class UserService : IUserService
 {
-    private readonly IReadOnlyList<User> users = GenerateUsers(10000);
+    private static readonly string UserFilePath = Path.Combine(
+        Path.GetDirectoryName(Environment.ProcessPath)!, 
+        "data", "users.csv");
+    private static readonly int NoOfUsers = 20000;
 
-    public async Task<string> GetRandomUserName() => users[Random.Shared.Next(users.Count)].UserName;
+    public async Task<string> GetRandomUserName() => 
+        GetUserName(Random.Shared.Next(NoOfUsers) + 1);
 
-    public async Task<IReadOnlyList<User>> GetUsers() => users;
+    public async Task<IReadOnlyList<User>> GetUsers()
+    {
+        if (!File.Exists(UserFilePath))
+            await CreateUserFile();
 
-    private static IReadOnlyList<User> GenerateUsers(int numberOfUsers) =>
-        Enumerable.Range(1, numberOfUsers)
+        var users = new List<User>(NoOfUsers);
+        foreach (var line in await File.ReadAllLinesAsync(UserFilePath))
+        {
+            var parts = line.Split(",");
+            users.Add(new User(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]));
+        }
+
+        return users;
+    }
+
+    private static async Task CreateUserFile()
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(UserFilePath)!);
+    
+            var users =
+                GenerateUsers(NoOfUsers)
+                    .Select(u => string.Join(",", u.UserName, u.FirstName, u.LastName, u.Address, u.City, u.Country));
+            await File.WriteAllLinesAsync(UserFilePath, users);
+        
+    }
+
+    private static IEnumerable<User> GenerateUsers(int numberOfUsers)
+    {
+        return Enumerable.Range(1, numberOfUsers)
             .Select(i => new User(
-                        $"user {i}",
-                        Name.First(),
-                        Name.Last(),
-                        Address.StreetAddress(),
-                        Address.City(),
-                        Address.Country()
-                    ))
-            .ToList();
+                GetUserName(i),
+                Name.First(),
+                Name.Last(),
+                Address.StreetAddress(),
+                Address.City(),
+                Address.Country()
+            ));
+    }
+
+    private static string GetUserName(int i)
+    {
+        return $"user {i}";
+    }
 }
